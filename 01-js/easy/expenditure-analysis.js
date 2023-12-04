67d6d6cf5b4d9fa891a71d058cb43d57a4e3a81b
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-12-04 18:35:37 +0530

    solution: expenditure analysis code

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,90 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const ans = [];
+  for(let i = 0; i < transactions.length; i++){
+    let trans = transactions[i];
+    let category = trans.category;
+    let price = trans.price;
+    let obj = contains(ans,category);
+
+    if(obj!=null){
+      obj.totalSpent += price;
+    }else{
+      ans.push({'category':category, 'totalSpent':price});
+    }
+  }
+
+  function contains(ans, category){
+    for(let i = 0; i<ans.length;i++){
+      if(ans[i].category==category){
+        return ans[i];
+      }
+    }
+    return;
+  }
+  return ans;
 }
+
+//
+// const transactions = [
+//   {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 10,
+//     category: 'Food',
+//     itemName: 'Pizza',
+//   },
+//   {
+//     id: 2,
+//     timestamp: 1656259600000,
+//     price: 20,
+//     category: 'Food',
+//     itemName: 'Burger',
+//   },
+//   {
+//     id: 3,
+//     timestamp: 1656019200000,
+//     price: 15,
+//     category: 'Clothing',
+//     itemName: 'T-Shirt',
+//   },
+//   {
+//     id: 4,
+//     timestamp: 1656364800000,
+//     price: 30,
+//     category: 'Electronics',
+//     itemName: 'Headphones',
+//   },
+//   {
+//     id: 5,
+//     timestamp: 1656105600000,
+//     price: 25,
+//     category: 'Clothing',
+//     itemName: 'Jeans',
+//   },
+// ];
+
+// let valCat = transactions[0]?.category
+// console.log(valCat);
+// let tempArr = [];
+// for(let i = 0; i < transactions.length; i++){
+//   tempArr.push({
+//     'category': transactions[i].category,
+//     'price':transactions[i].price
+//   });
+// }
+// console.log(tempArr);
+// for(let i = 0; i < tempArr.length; i++){
+//   if(tempArr[i]?.category){
+//     //google update value with specific key
+//   }else{
+//     ans.push({
+//       'category': transactions[i].category,
+//       'price':transactions[i].price
+//     });
+//   }
+// }
+//
 
 module.exports = calculateTotalSpentByCategory;
