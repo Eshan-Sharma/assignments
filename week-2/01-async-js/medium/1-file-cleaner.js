e08c234a66f8c57153988188d36cdc1c04b0505b
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-12-12 12:05:12 +0530

    add:remove space between words

--- week-2/01-async-js/medium/1-file-cleaner.js
+++ week-2/01-async-js/medium/1-file-cleaner.js
@@ -1,1 +1,48 @@
 const fs = require('fs');
+let filePath = 'diary.txt';
+
+function reading(){
+    let read = new Promise((resolve, reject)=>{
+        fs.readFile(filePath, 'utf-8', (err, data)=>{
+            if(err){
+                reject();
+                console.log(err);
+            }else{
+                resolve(data);
+                // console.log(data);
+            }
+        })
+    });
+    return read;
+}
+
+
+function writing(data){
+    return new Promise((resolve, reject)=>{
+        fs.writeFile(filePath, data, (err)=>{
+            if(err){
+                reject();
+                console.log(err);
+            }else{
+                console.log('Written:');
+                console.log(fs.readFileSync(filePath, "utf-8"));
+                
+                resolve();
+            }
+        })
+    })
+}
+
+async function removeSpace(){
+    let data = await reading().then();
+    console.log("Original Data: \n"+data);
+    let parsedData =  data.replace(/\s+/g, " ");
+    console.log('Parsed Data: \n'+parsedData);
+    // console.log(data);
+    await writing(parsedData);
+    console.log('End');
+      
+
+}
+
+removeSpace()
