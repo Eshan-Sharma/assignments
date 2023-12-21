1ac2f4e8b9a0b8179714bb6ba5f1de19a8f45a10
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-12-21 15:24:29 +0530

    add: jwt token solution added

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
 
-
+const zod = require("zod");
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
 /**
  * Generates a JWT for a given username and password.
  *
@@ -15,6 +17,16 @@ const jwtPassword = 'secret';
  */
 function signJwt(username, password) {
     // Your code here
+    const validUsername = emailSchema.safeParse(username);
+    const validPassword = passwordSchema.safeParse(password);
+    let signature;
+    if(!validPassword.success || !validUsername.success){
+        //res.send();
+        return null;
+    }else{
+        signature = jwt.sign({username}, jwtPassword);
+    }
+    return signature;
 }
 
 /**
@@ -27,6 +39,12 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+    try{
+        jwt.verify(token, jwtPassword);
+    }catch(e){
+        return false;
+    }
+    return true;
 }
 
 /**
@@ -38,6 +56,12 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+    const decoded = jwt.decode(token);
+    if(decoded){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 
