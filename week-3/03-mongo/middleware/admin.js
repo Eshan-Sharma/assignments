34ea4f2c9f0a7175e03c560c09ee245b61ddd855
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-12-26 18:47:11 +0530

    middleware logic added

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,18 @@
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    try{
+        const { username, password } = req.headers;
+        const admin = await Admin.findOne({ username, password});
+        if(!admin){
+            return res.status(401).json({message:"Unauthorized access"});
+        }
+        req.admin = admin;
+        next();
+    }catch(err){
+        res.status(500).json({message: "Internal Server Error"});
+    }
 }
 
 module.exports = adminMiddleware;
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,17 @@
-function userMiddleware(req, res, next) {
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    try{
+        const { username, password } = req.headers;
+        const user = await User.findOne({username, password});
+        if(!user){
+            return res.status({message: "Unauthorized access"});
+        }
+        req.user = user;
+        next();
+    }catch(err){
+        res.status(500).json({message: "Internal server Error"});
+    }
 }
 
 module.exports = userMiddleware;
