646dea1ffa98f8465caf1ab204d4126f89c32c05
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-12-26 19:47:28 +0530

    routes added

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -3,20 +3,43 @@ const adminMiddleware = require("../middleware/admin");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    try{
+        const {username, password} = req.headers;
+        const admin = new Admin({username, password, role:"admin"});
+        await admin.save();
+        res.status(201).send("Admin created successfully");
+    }catch(err){
+        res.status(500).send(err.message);
+    }
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
-});
-
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware,async (req, res) => {
     // Implement course creation logic
+    try{
+        const {title, price} = req.headers;
+        const course = new Course({title, price});
+        await course.save();
+        res.status(201).send("Course created successfully");
+    }catch(err){
+        res.status(500).send(err.message)
+    }
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    try{
+        const {username, password} = req.headers;
+        const admin = await Admin.findOne({username, password});
+        if(!admin){
+            return res.status(401).json({message:"Unauthorized access"});
+        }
+        const courses = await Course.find();
+        res.status(200).json(courses);
+    }catch(err){
+        res.status(500).send(err.message);
+    }
 });
 
 module.exports = router;
--- week-3/04-mongo-with-jwt-auth/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -3,24 +3,51 @@ const router = Router();
 const userMiddleware = require("../middleware/user");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',async (req, res) => {
     // Implement user signup logic
+    try {
+        const {username, password} = req.headers;
+        const existingUser = await User.findOne({username});
+        if(existingUser){
+            res.status(400).json({message:"User already exists"});
+        }
+        const user = new User({username, password});
+        await user.save();
+        res.status(201).json({message:"User created successfully"});
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
-});
-
-router.get('/courses', (req, res) => {
+router.get('/courses',async (req, res) => {
     // Implement listing all courses logic
+    try{
+        const courses = await Courses.find();
+        res.status(200).json(courses);
+    }catch(err){
+        res.status(500).send({message:"Internal server error"});
+    }
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
+    try{
+        const courseId = req.params.courseId;
+        res.status(200).json({message: "Course purchased successfully"});
+    }catch(err){
+        res.status(500).json({message: "Internal server Error"});
+    }
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    try{
+        const userId = req.user._Id;
+        res.status(200).json({purchasedCourses:[]});
+    }catch(err){
+        res.status(500).send(err.message);
+    }
 });
 
 module.exports = router
