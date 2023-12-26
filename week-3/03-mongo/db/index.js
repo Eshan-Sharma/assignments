3e502ac6af367a08528a9ed5e69a97923a6bdda5
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-12-26 18:35:50 +0530

    schema and url defined

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,30 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://eshansharma108:mjkYLw1BtcKhOYRS@cluster0.vadnd9b.mongodb.net/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String, 
+    password: String, 
+    role: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String, 
+    password: String,
+    role: String
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String, 
+    description: String, 
+    price: Number, 
+    imageLink: String,
+    published: Boolean
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
