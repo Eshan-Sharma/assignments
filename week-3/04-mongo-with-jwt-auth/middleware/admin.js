5c315a1f5ac4bde526613f2a3bba37c5a5e61daf
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-12-26 19:43:17 +0530

    connection link and auth logic added

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,25 @@
+const jwt = require('jsonwebtoken');
+const jwtSecret = "secret";
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    try{
+        const token = req.headers.authorization;
+        if(!token){
+            return res.status(401).json({message:"Unauthorized access"});
+        }
+        const decoded = jwt.verify(token, jwtSecret);
+        const { username, password } = req.headers;
+        const admin = await Admin.findById(decoded.userId);
+        if(!admin){
+            return res.status(401).json({message:"Unauthorized access"});
+        }
+        req.admin = admin;
+        next();
+    }catch(err){
+        res.status(500).json({message: "Internal Server Error"});
+    }
 }
 
 module.exports = adminMiddleware;
--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,24 @@
-function userMiddleware(req, res, next) {
+const jwt = require('jsonwebtoken');
+const jwtSecret = "secret";
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    try{
+        const token = req.headers.authorization;
+        if(!token){
+            return res.status(401).json({message:"Unauthorized access"});
+        }
+        const decoded = jwt.verify(token, jwtSecret);
+        const { username, password } = req.headers;
+        const user = await User.findById(decoded.userId);
+        if(!user){
+            return res.status({message: "Unauthorized access"});
+        }
+        req.user = user;
+        next();
+    }catch(err){
+        res.status(500).json({message: "Internal server Error"});
+    }
 }
 
 module.exports = userMiddleware;
