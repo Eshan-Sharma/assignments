8b318b73886c49b3d288ce0533c20fa27b5c00bd
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-01-12 21:10:15 +0530

    fixed rendering all todos

--- todo-fullstack/frontend/src/components/Todos.jsx
+++ todo-fullstack/frontend/src/components/Todos.jsx
@@ -1,11 +1,12 @@
-export function Todos({todos}){
+export function Todos({todos}) {
+
     return <div>
-       {todos.map(function(todo){
-        return <div>
-            <h1>{todo.title}</h1>
-            <h3>{todo.description}</h3>
-            <button>{todo.completed == true ? "Completed": "Mark as done!"}</button>
-        </div>
-       })}
+        {todos.map(function(todo) {
+            return <div>
+                <h1>{todo.title}</h1>
+                <h2>{todo.description}</h2>
+                <button>{todo.completed == true ? "Completed" : "Mark as Complete"}</button>
+            </div>
+        })}
     </div>
 }
